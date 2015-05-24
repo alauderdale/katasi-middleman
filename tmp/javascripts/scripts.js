595acee4b791467c0fe6fba664d84b1c0d45e088
Alex Lauderdale <alauderdale@mac.com>
2015-05-23 22:36:06 -0600

    blog

--- tmp/javascripts/scripts.js
+++ tmp/javascripts/scripts.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 
 	 //fancybox
-	$('.fancybox').fancybox();
+	$('.fancybox').fancybox({
+        padding: 0
+    });
 	
 	$('.fb-video').fancybox({
         padding: 0,
@@ -19,7 +21,7 @@ $(document).ready(function(){
 //smooth scroll
 
 	$(function() {
-		  $('a[href*=#]:not([href=#])').click(function() {
+		  $('a[href*=#ss]:not([href=#])').click(function() {
 		    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 		      var target = $(this.hash);
 		      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -38,14 +40,39 @@ $(document).ready(function(){
 
 
 	 $(window).scroll(function(){
-     var divOffset = $('.menu-change').offset();
+     var divOffset = $('section :first-of-type').offset();
      if(window.scrollY > divOffset.top ){
-        $('.main-nav').addClass('small-nav');
+        $('.main-nav').addClass('scrolled-nav');
      }
      if(window.scrollY < divOffset.top){
-        $('.main-nav').removeClass('small-nav');
+        $('.main-nav').removeClass('scrolled-nav');
      }
     });
+
+
+
+   //slider
+
+
+       $('#logo-slider').lightSlider({
+        gallery:true,
+        item:1,
+        loop:true,
+        mode:'fade',
+        slideMargin:30,
+        thumbMargin:10,
+        thumbItem:5,
+        enableDrag: false,
+        keyPress:true,
+        controls:false,
+        currentPagerPosition:'middle',
+        onSliderLoad: function(el) {
+            el.lightGallery({
+                selector: '#imageGallery .lslide'
+            });
+        }   
+    });  
+
 
 
 });
